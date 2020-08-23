dd60951b83a055e5ed194065fb2df2334837049c
Jerzy <jgrynczewski@gmail.com>
2020-08-23 06:48:40 +0200

    ci9

--- ci_cd/settings.py
+++ ci_cd/settings.py
@@ -9,18 +9,17 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ly97e#bh!*%8!*=u7jumec3uiq&-g^k8^sygkrd&(vnb#df^$%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
